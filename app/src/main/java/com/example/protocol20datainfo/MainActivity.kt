d41af5925d1429ba71bbf8563e511821b2ad99f3
choco5732 <choco5732@gmail.com>
2024-04-18 14:43:44 +0900

    splash api 적용 , 아이콘은 uxn으로

--- app/src/main/java/com/example/protocol20datainfo/MainActivity.kt
+++ app/src/main/java/com/example/protocol20datainfo/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.protocol20datainfo
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.protocol20datainfo.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +18,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 200L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
+        }
+
 
         binding.mainHelloTv.setOnClickListener {
             if (isBlue) {
