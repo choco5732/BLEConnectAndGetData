567f4bb8aae61c686c34dedad8f44ffa19338d68
choco5732 <choco5732@gmail.com>
2024-04-18 17:27:24 +0900

    ble작업, 액티비티 에러수정

--- app/src/main/java/com/example/protocol20datainfo/MainActivity.kt
+++ app/src/main/java/com/example/protocol20datainfo/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.protocol20datainfo
 
+import android.Manifest
 import android.animation.ObjectAnimator
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.AnticipateInterpolator
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.protocol20datainfo.databinding.MainActivityBinding
 
@@ -51,5 +60,58 @@ class MainActivity : AppCompatActivity() {
             }
             isBlue = !isBlue
         }
+
+
+        // Bluetooth매니저 : 블루투스어댑터를 만들수 있다.
+        val bleManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        // Bluetooth어댑터 : 장치검색,
+        val bluetoothAdapter = bleManager.adapter
+
+//        val targetAddress = "60:C0:BF:ED:5E:DF"
+//        val device : BluetoothDevice = bluetoothAdapter.getRemoteDevice(targetAddress)
+
+        var device : BluetoothDevice? = null
+
+        val callback : ScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult?) {
+                super.onScanResult(callbackType, result)
+
+                device = result?.device
+
+                Log.d("choco5732", "scanning : ${device.toString()}")
+            }
+
+            override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+                super.onBatchScanResults(results)
+            }
+
+            override fun onScanFailed(errorCode: Int) {
+                super.onScanFailed(errorCode)
+                Log.d("choco5732", "errorCode : ${errorCode}")
+            }
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            Log.d("choco5732", "not grantted")
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        bluetoothAdapter.getBluetoothLeScanner().startScan(callback)
+
+
+
+
     }
 }
